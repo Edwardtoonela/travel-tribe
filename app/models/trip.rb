95cb76cedfc8ffeaad6e352e139091a70ac69649
Daniel Costa <dancosta.id@gmail.com>
2022-03-10 14:57:38 -0300

    added relations to models and added routes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,8 @@
 class Trip < ApplicationRecord
+  has_many :bookmarks
+  has_many :bookings
+  has_many :reviews
+
+  validates :description, presence: true
+  validates :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  has_many :bookmarks
+  has_many :bookings
+  has_many :reviews
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fullname, presence: true, length: { minimum: 5 }
+  validates :username, presence: true, length: { minimum: 3 }
 end
