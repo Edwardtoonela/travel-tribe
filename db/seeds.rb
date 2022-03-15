d3b937875c3dee0e5fe07dea8ce986554b80f1d4
Daniel Costa <dancosta.id@gmail.com>
2022-03-15 14:59:53 -0300

    I've worked on the backend and a bit of styling for the trips page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,12 @@ User.create!(full_name: 'Fabio', email: "fabio@gmail.com", password: '123456', u
 puts "Adding dummy trip"
 Trip.create!(name: "Chill trip to Amsterdam", start_date: '05-07-2022', end_date: "05-22-2022", description: 'Best trip ever i can not belivev asdkfnskd fksdnf', location: 'Madafuckinggaskar', max_guests: "7", price_min: "500", price_max: "700")
 Trip.create!(name: "India awaits", start_date: '07-11-2022', end_date: "07-12-2022", description: 'Well this is going to be a big one. We are going to India for a month I can not believe it. You have to see this with your own eyes.', location: 'India', max_guests: "4", price_min: "1500", price_max: "1900")
+Trip.create!(name: "Queenstown, NZ", start_date: '09-05-2022', end_date: "10-06-2022", description: 'The coolest trip you one can do. We are going to Queenstown for a month I can not believe it. You have to see this with your own eyes.', location: 'New Zealand', max_guests: "6", price_min: "2500", price_max: "3500")
 puts "Adding dummy ratings"
-Review.create!(comment: "Horrible. I cant recommend this to anyone", rating: '5', user: User.first, trip: Trip.first)
-Review.create!(comment: "Horrible. I cant recommend this to anyone", rating: '4', user: User.first, trip: Trip.last)
-Review.create!(comment: "Horrible. I cant recommend this to anyone", rating: '2', user: User.first, trip: Trip.first)
+Review.create!(comment: "Unbelivebly good. I will recommend this to everyone", rating: '5', user: User.first, trip: Trip.first)
+Review.create!(comment: "Pretty Good. Very satisfied with it.", rating: '4', user: User.first, trip: Trip.last)
+Review.create!(comment: "Poor. I cant recommend this to anyone", rating: '2', user: User.first, trip: Trip.first)
 Review.create!(comment: "Horrible. I cant recommend this to anyone", rating: '1', user: User.first, trip: Trip.last)
-Review.create!(comment: "Horrible. I cant recommend this to anyone", rating: '3', user: User.first, trip: Trip.first)
+Review.create!(comment: "Avarage. Now sure if I can recommend this to anybody", rating: '3', user: User.first, trip: Trip.first)
 Review.create!(comment: "Horrible. I cant recommend this to anyone", rating: '2', user: User.first, trip: Trip.last)
 puts "Finished"
