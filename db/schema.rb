181ead7862a52fe4bc0ab0dfb3303a56abf6b9ea
Daniel Costa <dancosta.id@gmail.com>
2022-03-16 11:06:45 -0300

    removed nickname column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_104414) do
+ActiveRecord::Schema.define(version: 2022_03_16_140548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 2022_03_16_104414) do
     t.string "full_name"
     t.string "user_name"
     t.string "tags"
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
