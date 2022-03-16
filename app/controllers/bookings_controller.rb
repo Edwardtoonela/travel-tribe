4a52c0eaf618d29799133abc1a74bac59dacd6d9
Edward Toonela <edward.toonela@icloud.com>
2022-03-16 14:31:23 -0300

    added bookings controller methods and connected button on trip show page.Checking out to merge master

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @trip = Trip.find(params[:trip_id])
+    @booking.trip = @trip
+    @booking.user = current_user
+
+    authorize @booking
+
+    if @booking.save
+      redirect_to trip_path(@trip)
+      flash[:notice] = "Your booking is done"
+    else
+      flash[:alert] = "Oops!! You need to fill in the form!"
+      render 'trips/show'
+    end
+  end
+
+  def destroy
+    authorize current_user
+    @booking = Booking.find(booking)
+    @booking.destroy
+    redirect_to users,
+    flash[:notice] = "Your booking was successfully canceled."
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit([:start_date, :end_date])
+  end
 end
