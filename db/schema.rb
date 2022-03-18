3797d86c51d45a0bbcd227610e72020a4cf05698
Daniel Costa <dancosta.id@gmail.com>
2022-03-18 16:19:36 -0300

    Removed location and replaced it with address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_111114) do
+ActiveRecord::Schema.define(version: 2022_03_18_191633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 2022_03_18_111114) do
     t.date "start_date"
     t.date "end_date"
     t.string "description"
-    t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
