4a6305b701969de2e4a9cc568871a1f702330127
Daniel Costa <dancosta.id@gmail.com>
2022-03-18 12:03:26 -0300

    Pop ain't working yet.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     if params[:query].blank?
       @trips = policy_scope(Trip)
     else
-     # @parameter = trip_params[:search].downcase
       @trips = policy_scope(Trip).algolia_search(params[:query])
       ids = @trips.map do |trip|
         trip.id
@@ -13,12 +12,12 @@ class PagesController < ApplicationController
       @trips = Trip.where(id: ids)
     end
 
-    # the `geocoded` scope filters only trips with coordinates (latitude & longitude)
+    # Map Markers
     @markers = @trips.geocoded.map do |trip|
       {
         lat: trip.latitude,
         lng: trip.longitude,
-        # info_window: render_to_string(partial: 'info_window', locals: { trip: trip })
+        # info_window: render_to_string(partial: 'pages/info_window', trip: trip )
       }
     end
   end
