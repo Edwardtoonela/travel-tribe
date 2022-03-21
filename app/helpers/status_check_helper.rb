8ba400523f77e566f95e4b9095b7b9dda72b6e06
Edward Toonela <edward.toonela@icloud.com>
2022-03-21 16:09:54 -0300

    user wont show up on homepage as well, adjusted helper method a little to do that.

--- app/helpers/status_check_helper.rb
+++ app/helpers/status_check_helper.rb
@@ -1,7 +1,7 @@
 module StatusCheckHelper
-  def check_status
+  def check_status(trip)
 
-    booking = @trip.bookings.where(user: current_user).first
+    booking = trip.bookings.where(user: current_user).first
     if !(booking.nil?) && booking.status == "confirmed"
       return true
     else
