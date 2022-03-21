615efec378411ef55fd650557e164b81457549c8
Edward Toonela <edward.toonela@icloud.com>
2022-03-21 16:17:27 -0300

    Added price back to card

--- app/helpers/average_price_helper.rb
+++ app/helpers/average_price_helper.rb
@@ -1,6 +1,6 @@
 module AveragePriceHelper
-  def average_price
-    sum_price = @trip.price_max + @trip.price_min
+  def average_price(trip)
+    sum_price = trip.price_max + trip.price_min
     average = sum_price / 2
     average
   end
