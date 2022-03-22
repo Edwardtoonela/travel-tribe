3afa3f8357091796418a29bd311fe1284207243c
Edward Toonela <edward.toonela@icloud.com>
2022-03-22 11:15:12 -0300

    fixed trip booking check status methods

--- app/helpers/status_check_helper.rb
+++ app/helpers/status_check_helper.rb
@@ -1,5 +1,10 @@
 module StatusCheckHelper
-  def check_status(trip)
+  def check_status(booking)
+
+    booking.status == "confirmed"
+  end
+
+  def current_user_booking_status(trip)
 
     booking = trip.bookings.where(user: current_user).first
     if !(booking.nil?) && booking.status == "confirmed"
@@ -8,5 +13,4 @@ module StatusCheckHelper
       return false
     end
   end
-
 end
